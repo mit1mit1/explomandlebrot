14c5b40c8b85e52325d84b7daa105265b7fe81a8
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-10-12 15:16:53 +1100

    feat: retry setting the flag

--- src/state.ts
+++ src/state.ts
@@ -6,6 +6,7 @@ import {
   gridZoomDivider,
 } from "./constants";
 import { setDepthPointer } from "./utils/depthPointer";
+import { calculateMandlenumber } from "./utils/math";
 
 export const characterPosition = {
   xSquare: Math.floor(xResolution / 2),
@@ -33,18 +34,74 @@ export const inputability = {
 
 const searchParams = new URLSearchParams(window.location.search);
 
-export const compassDestination = {
-  xPosition: parseFloat(
-    searchParams.get("xDestination") || (0.5 * Math.random() + 0.5).toString()
-  ),
-  yPosition: parseFloat(
-    searchParams.get("yDestination") || (0.5 * Math.random() + 0.5).toString()
-  ),
-};
-console.log(initialXStepDistance * gridZoomDivider)
+const paramXDestination = searchParams.get("xDestination");
+const paramYDestination = searchParams.get("yDestination");
+
 export const zoomDestination = {
   gridDistance: searchParams.get("zoomDestination")
     ? parseFloat(searchParams.get("zoomDestination") || "0")
     : initialXStepDistance * gridZoomDivider,
 };
 setDepthPointer(gridDistance.xStepDistance, zoomDestination.gridDistance);
+
+const isReachable = (
+  compassDestination: { xPosition: number; yPosition: number },
+  zoomDestination: { gridDistance: number }
+) => {
+  const destinationMandleNumber = calculateMandlenumber(
+    compassDestination.xPosition,
+    compassDestination.yPosition,
+    0,
+    0,
+    0
+  );
+  const adjacentMandleNumbers = [
+    calculateMandlenumber(
+      compassDestination.xPosition + zoomDestination.gridDistance,
+      compassDestination.yPosition,
+      0,
+      0,
+      0
+    ),
+    calculateMandlenumber(
+      compassDestination.xPosition - zoomDestination.gridDistance,
+      compassDestination.yPosition,
+      0,
+      0,
+      0
+    ),
+    calculateMandlenumber(
+      compassDestination.xPosition,
+      compassDestination.yPosition + zoomDestination.gridDistance,
+      0,
+      0,
+      0
+    ),
+    calculateMandlenumber(
+      compassDestination.xPosition,
+      compassDestination.yPosition - zoomDestination.gridDistance,
+      0,
+      0,
+      0
+    ),
+  ];
+  return adjacentMandleNumbers.some(
+    (number) => number !== destinationMandleNumber
+  );
+};
+
+export const compassDestination = {
+  xPosition: parseFloat(paramXDestination || "0"),
+  yPosition: parseFloat(paramYDestination || "0"),
+};
+
+if (!paramXDestination && !paramYDestination) {
+  let attemptsToSetCompass = 0;
+  while (
+    attemptsToSetCompass < 15 &&
+    !isReachable(compassDestination, zoomDestination)
+  ) {
+    compassDestination.xPosition = 0.5 + 0.5 * Math.random();
+    compassDestination.yPosition = 0.5 + 0.5 * Math.random();
+  }
+}
